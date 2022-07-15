f8ca6f698d57c03b665dd85b405f4407a0066d87
Bleona Gerbavci <bg52732@ubt-uni.net>
2022-07-15 17:52:28 +0200

    initial commit

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using eCommerce.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();    
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+
+    }
+}
---
+++ eCommerce/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options ) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
